f92fad0a47205277dde30e8945f760143d5ad7f6
Ryan Elston <dragonfanguy@gmail.com>
2024-05-12 17:54:48 -0700

    add lvim config

---
+++ .config/lvim/config.lua
@@ -0,0 +1,7 @@
+lvim.plugins = {
+  { "sainnhe/gruvbox-material", priority = 1000, config = true, opts = ... }
+}
+lvim.colorscheme = "gruvbox-material"
+
+-- remove tilde on empty lines
+vim.opt.fillchars = 'eob: '
